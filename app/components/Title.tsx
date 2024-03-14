7b4d7b4cf06c9e79cbd5ca47cef8674896509413
joaopereira <joaopereirawd@gmail.com>
2024-03-14 19:06:15 +0000

    Remove unnecessary files

--- app/components/Title.tsx
+++
@@ -1,9 +0,0 @@
-import styles from '../scss/headings.module.scss'
-
-export default function Title({ text }) {
-  return (
-    <h1 className={styles.h1}>
-      {text} yes
-    </h1>
-  )
-}
--- app/work/page.tsx
+++
@@ -1,9 +0,0 @@
-import Title from '../components/Title';
-
-export default function Work() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Title text="WorkPage "></Title>
-    </main>
-  )
-}
